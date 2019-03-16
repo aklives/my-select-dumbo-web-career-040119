19a0b4fcce797e5215c94fb693cec4c84854a49f
Adam Kearney <aklives@hotmail.com>
2019-03-16 10:35:28 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,16 +1,12 @@
-def my_select(collection)
-  i =0
-  select = []
-  if collection.empty? == false
-    while i < collection.length
-      if yield(collection[i]) == true
-        select << collection[i]
-        i += 1
-      end
-    end
-  else
-    return "Sorry, your array is empty"
-  end
-
-select
-end
+def my_select(array) 
+  i =0 
+  select = [] 
+  while i < array.length 
+    if (yield(array[i]))
+      # select.push(array[i])OR 
+      select << array[i] 
+    end 
+    i += 1 
+  end 
+  select 
+end 
