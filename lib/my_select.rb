d6d86c1a6751d63b249bc749fe1efc7a67073e17
Adam Kearney <aklives@hotmail.com>
2019-03-16 10:34:49 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+  i =0
+  select = []
+  if collection.empty? == false
+    while i < collection.length
+      if yield(collection[i]) == true
+        select << collection[i]
+        i += 1
+      end
+    end
+  else
+    return "Sorry, your array is empty"
+  end
+
+select
 end
