0bcfa42b8f1e9544bbf621d0d39b3b22aa523101
Adam Kearney <aklives@hotmail.com>
2019-03-16 10:35:31 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,12 @@
-def my_select(array) 
-  i =0 
-  select = [] 
-  while i < array.length 
+def my_select(array)
+  i =0
+  select = []
+  while i < array.length
     if (yield(array[i]))
-      # select.push(array[i])OR 
-      select << array[i] 
-    end 
-    i += 1 
-  end 
-  select 
-end 
+      # select.push(array[i])OR
+      select << array[i]
+    end
+    i += 1
+  end
+  select
+end
